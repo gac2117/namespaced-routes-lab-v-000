5d50e757d3f0c46914b331ed8b9ef92782e8d126
Grace Choi <oiograceoio@gmail.com>
2019-05-01 18:03:41 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :posts, only: [:index, :show]
+    resources :songs, only: [:index, :show]
   end
 
-  resources :posts
+  resources :songs
 
   namespace :admin do
     resources :preferences
